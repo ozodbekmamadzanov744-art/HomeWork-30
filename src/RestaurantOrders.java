3fafae99d497a2627bfda860b292680d5aff246e
Озодбек <ozodbekmamadzanov744@gmail.com>
2026-05-05 17:54:29 +0600

    Add methods for Task 2: grouping orders by customer and items

--- src/RestaurantOrders.java
+++ src/RestaurantOrders.java
@@ -1,4 +1,5 @@
 import com.google.gson.Gson;
+import domain.Item;
 import domain.Order;
 
 
@@ -7,6 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
@@ -102,11 +104,53 @@ public class RestaurantOrders {
     }
 
     public List<String> getSortedUniqueEmails() {
-        return orders.stream()
+        return orders
+                .stream()
                 .map(o -> o.getCustomer().getEmail())
                 .collect(Collectors.toCollection(TreeSet::new))
                 .stream()
                 .collect(Collectors.toList());
+    }
+
+
+    public Map<String, List<Order>> getOrdersGroupedByCustomer() {
+        return orders.stream()
+                .collect(Collectors.groupingBy(o -> o.getCustomer().getFullName()));
+    }
+
+
+    public Map<String, Double> getTotalByCustomer() {
+        return orders.stream()
+                .collect(Collectors.groupingBy(
+                        o -> o.getCustomer().getFullName(),
+                        Collectors.summingDouble(Order::getTotal)
+                ));
+    }
+
+
+    public String getCustomerWithMaxTotal() {
+        return getTotalByCustomer().entrySet().stream()
+                .reduce((a, b) -> a.getValue() >= b.getValue() ? a : b)
+                .map(Map.Entry::getKey)
+                .orElse(null);
+    }
+
+
+    public String getCustomerWithMinTotal() {
+        return getTotalByCustomer().entrySet().stream()
+                .reduce((a, b) -> a.getValue() <= b.getValue() ? a : b)
+                .map(Map.Entry::getKey)
+                .orElse(null);
+    }
+
+
+    public Map<String, Integer> getSoldAmountByItem() {
+        return orders.stream()
+                .flatMap(o -> o.getItems().stream())
+                .collect(Collectors.groupingBy(
+                        Item::getName,
+                        Collectors.summingInt(Item::getAmount)
+                ));
     }
 
 
